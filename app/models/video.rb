2a80fc5029966c7f7548e3f9e70e942d5dc5fa46
Simon Courtois <scourtois+github@cubyx.fr>
2014-01-31 15:12:26 +0100

    Setting the video slug automatically

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,13 @@
 class Video < ActiveRecord::Base
+  before_save :set_slug
+
   def vimeo_id
     vimeo_url[/vimeo.com\/(\d+)/, 1]
+  end
+
+  private
+
+  def set_slug
+    self.slug = title.parameterize if slug.blank?
   end
 end
