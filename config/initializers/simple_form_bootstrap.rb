700f6f2f9c9acd82ef2a1bdd8d0edc6e7e38525c
Simon Courtois <scourtois_github@cubyx.fr>
2014-02-20 22:22:42 +0100

    Tuning the login/password recovery pages

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -24,5 +24,15 @@ SimpleForm.setup do |config|
     b.optional :pattern
   end
 
+  config.wrappers :checkbox, tag: :div, class: 'checkbox', error_class: 'has-error' do |b|
+    b.use :html5
+    b.wrapper tag: 'label', class: 'col-sm-9 col col-sm-offset-3' do |ba|
+      ba.use :input
+      ba.use :label_text
+    end
+    b.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+    b.use :error, wrap_with: { tag: :span, class: "help-block text-danger" }
+  end
+
   config.default_wrapper = :bootstrap_horizontal
 end
