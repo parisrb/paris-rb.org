b8e23c75f8eed961ac56e47375dae66784b8e741
Simon Courtois <scourtois_github@cubyx.fr>
2014-02-21 14:06:08 +0100

    Customizing the talks admin section

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,4 +3,39 @@ RailsAdmin.config do |config|
   config.authenticate_with { warden.authenticate! scope: :user }
   config.current_user_method(&:current_user)
   config.default_items_per_page = 30
+
+  config.model 'Talk' do
+    list do
+      field :happened do
+        formatted_value do
+          if bindings[:object].happened_at
+            bindings[:view].tag(:span, class: 'label label-success') << I18n.t('yes')
+          else
+            bindings[:view].tag(:span, class: 'label label-important') << I18n.t('no')
+          end
+        end
+      end
+      field :title
+      field :speaker_name
+      field :duration
+      field :slides do
+        formatted_value do
+          if bindings[:object].slides?
+            bindings[:view].tag(:span, class: 'label label-success') << I18n.t('yes')
+          else
+            bindings[:view].tag(:span, class: 'label label-important') << I18n.t('no')
+          end
+        end
+      end
+      field :video_slug do
+        formatted_value do
+          if bindings[:object].video_slug?
+            bindings[:view].tag(:span, class: 'label label-success') << I18n.t('yes')
+          else
+            bindings[:view].tag(:span, class: 'label label-important') << I18n.t('no')
+          end
+        end
+      end
+    end
+  end
 end
