26167df4daff7e1e9aabce6245c5b6994a25fd71
Simon Courtois <scourtois_github@cubyx.fr>
2014-07-02 18:40:43 +0200

    Adding Simon and Sylvain as new talk mail recipients

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,8 +1,10 @@
 class TalkMailer < ActionMailer::Base
-  default from: "no-reply@rubyparis.org"
+  default from: 'no-reply@rubyparis.org'
 
   def new_talk(talk)
     @talk = talk
-    mail(to: 'thibaut@milesrock.com', from: talk.speaker_email, subject: '[Paris.rb] New Talk: ' + talk.title) # TODO: add recepients
+    mail to: ['thibaut@milesrock.com', 'scourtois@cubyx.fr', 'sylvain.abelard+rails@gmail.com'],
+         from: talk.speaker_email,
+         subject: "[Paris.rb] New Talk: #{talk.title}"
   end
 end
