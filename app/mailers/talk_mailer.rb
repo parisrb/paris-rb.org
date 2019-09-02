783d90f907c763022ce4abb38c39a9337baf8fe1
Yann Klis <yann.klis@gmail.com>
2019-09-02 10:30:47 +0200

    replaced hard coded recipient emails list by env var fix #58

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,7 +1,7 @@
 class TalkMailer < ApplicationMailer
   def new_talk(talk_id)
     @talk = Talk.find(talk_id)
-    mail to: ['thibaut@milesrock.com', 'scourtois@cubyx.fr', 'sylvain.abelard+rails@gmail.com'],
+    mail to: ENV['EMAIL_RECIPIENTS'],
          reply_to: @talk.speaker_email,
          subject: "[Paris.rb] New Talk: #{@talk.title}"
   end
