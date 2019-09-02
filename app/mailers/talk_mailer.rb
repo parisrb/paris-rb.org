b9a02598d8614fbfc2d33cb219148f1dd5cbe475
Yann Klis <yann.klis@gmail.com>
2019-09-02 10:32:54 +0200

    Workers -> Active Jobs (#61)
    * transformed workers to active job syntax and expected files hierarchy, deleted useless worker
    
    * replaced hard coded recipient emails list by env var fix #58

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,8 +1,8 @@
 class TalkMailer < ApplicationMailer
-  def new_talk(talk)
-    @talk = talk
-    mail to: ['thibaut@milesrock.com', 'scourtois@cubyx.fr', 'sylvain.abelard+rails@gmail.com'],
-         reply_to: talk.speaker_email,
-         subject: "[Paris.rb] New Talk: #{talk.title}"
+  def new_talk(talk_id)
+    @talk = Talk.find(talk_id)
+    mail to: ENV['EMAIL_RECIPIENTS'],
+         reply_to: @talk.speaker_email,
+         subject: "[Paris.rb] New Talk: #{@talk.title}"
   end
 end
