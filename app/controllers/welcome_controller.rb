38fde9fd744cddb08ead3c41c6588d552a5e2459
Jean Bertrand <jean@scalingo.com>
2023-01-04 21:50:10 +0100

    feat: remove link to deleted service (#167)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,5 @@ require 'meetup/members_count'
 class WelcomeController < ApplicationController
   expose(:members_count) { Meetup::MembersCount.total }
   expose(:sponsors) { Sponsor.visible }
-  expose(:job_offers) { JobOffer.all }
   expose(:tweets) { Tweet.all }
 end
