022791feb8fcb3945e8cbfce3bf47ead9eb7013e
Adrien Poly <adrienpoly@gmail.com>
2023-01-24 12:37:52 +0100

    Improve the sponsors logo image (#176)
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ require 'meetup/members_count'
 
 class WelcomeController < ApplicationController
   expose(:members_count) { Meetup::MembersCount.total }
-  expose(:sponsors) { Sponsor.visible }
+  expose(:sponsors) { Sponsor.latest(4) }
   expose(:tweets) { Tweet.all }
 end
--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -15,5 +15,6 @@
 class Sponsor < ApplicationRecord
   mount_uploader :logo, PictureUploader
 
-  scope :visible, ->{ where('until >= ? OR until IS NULL', Time.current) }
+  scope :current, ->{ where('until >= ? OR until IS NULL', Time.current) }
+  scope :latest,  -> (count) { order(until: :desc).limit(count) }
 end
