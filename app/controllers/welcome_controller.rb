9969ca4a367e495af5bac399f649db19411e4bd4
Adrien Poly <adrienpoly@gmail.com>
2023-02-02 18:40:41 +0100

    Delete dynamic Meetup counter (#178)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
-require 'meetup/members_count'
-
 class WelcomeController < ApplicationController
-  expose(:members_count) { Meetup::MembersCount.total }
   expose(:sponsors) { Sponsor.latest(4) }
   expose(:tweets) { Tweet.all }
 end
