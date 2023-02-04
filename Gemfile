5052196cd76bd239c45024d458db7e2ac24ccf4b
Adrien Poly <adrienpoly@gmail.com>
2023-02-04 14:15:12 +0100

    Add Tailwind and basic new layout for the redesign (#182)
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem "turbo-rails"
 # gem "stimulus-rails"
 
 # Use Tailwind CSS [https://github.com/rails/tailwindcss-rails]
-# gem "tailwindcss-rails"
+gem "tailwindcss-rails"
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action do
+    # This will need to be removed once we have transitioned to the new design
+    # during the transition period we can visit any page and add ?new=true to
+    # see the new design (if it exists)
+
+    if params[:new] == 'true'
+      session[:site_design] = :new
+    elsif params[:new] == 'false'
+      session[:site_design] = nil
+    end
+
+    request.variant = session[:site_design]&.to_sym
+  end
 end
