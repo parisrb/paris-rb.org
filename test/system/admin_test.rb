e35c972c79a865d3f2c7dbc4e28a4330908f44d0
Adrien Poly <adrienpoly@gmail.com>
2023-02-04 13:51:00 +0100

    Add missing translations for rails admin and basic tests for admin pages (#180)
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

---
+++ test/system/admin_test.rb
@@ -0,0 +1,36 @@
+require "application_system_test_case"
+
+class AdminTest < ApplicationSystemTestCase
+  setup do
+    @admin = users(:admin)
+  end
+
+  test "sign in admin and visit the admin" do
+    visit rails_admin.dashboard_path
+    fill_in "user[email]", with: @admin.email
+    fill_in "user[password]", with: "password"
+    click_on "Me connecter"
+    assert_selector "h1", text: "Administration"
+  end
+
+  class AuthenticatedAdminTest < ApplicationSystemTestCase
+    setup do
+      login_as users(:admin), scope: :user
+    end
+
+    test "visit the sponsors" do
+      visit rails_admin.index_path("sponsor")
+      assert_selector "h1", text: "Liste des Sponsor"
+    end
+
+    test "visit the videos" do
+      visit rails_admin.index_path("video")
+      assert_selector "h1", text: "Liste des Video"
+    end
+
+    test "visit the talks" do
+      visit rails_admin.index_path("talk")
+      assert_selector "h1", text: "Liste des Talk"
+    end
+  end
+end
