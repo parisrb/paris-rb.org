34fa22c5cb28a4430cc0b126a8099ba87d475fef
Adrien Poly <adrienpoly@gmail.com>
2023-02-04 13:23:54 +0100

    Update app/controllers/application_controller.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action do
-    # This will need to be removed once we have transition to the new design
+    # This will need to be removed once we have transitioned to the new design
     # during the transition period we can visit any page and add ?new=true to
     # see the new design (if it exists)
 
