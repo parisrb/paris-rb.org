80f95c23993e8d75b178af992a8501e2f945dedd
Adrien POLY <adrienpoly@gmail.com>
2024-04-02 14:45:15 +0200

    use Cloudinary with ActiveStorage

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,6 @@
 Cloudinary.config do |config|
   config.cloud_name = "dhj1cqakd"
+  config.api_key = ENV["CLOUDINARY_API_KEY"]
+  config.api_secret = ENV["CLOUDINARY_API_SECRET"]
   config.secure = true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_18_111323) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_02_114104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "rails_admin_histories", force: :cascade do |t|
     t.text "message"
@@ -21,19 +49,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_18_111323) do
     t.string "table"
     t.integer "month", limit: 2
     t.bigint "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["item", "table", "month", "year"], name: "index_rails_admin_histories"
   end
 
   create_table "sponsors", force: :cascade do |t|
     t.string "name"
     t.string "website"
-    t.string "logo"
-    t.datetime "from"
-    t.datetime "until"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "from", precision: nil
+    t.datetime "until", precision: nil
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "talks", force: :cascade do |t|
@@ -42,14 +69,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_18_111323) do
     t.string "speaker_email"
     t.string "level"
     t.string "duration"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.date "happened_at"
     t.string "slides"
     t.string "video_url"
     t.string "speaker_twitter"
     t.string "preferred_month_talk"
-    t.datetime "time_position"
+    t.datetime "time_position", precision: nil
     t.string "priority"
     t.index ["happened_at"], name: "index_talks_on_happened_at"
   end
@@ -59,18 +86,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_18_111323) do
     t.string "author"
     t.string "twitter_handle"
     t.string "cool_projects"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -82,9 +109,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_18_111323) do
     t.text "description"
     t.date "event_date"
     t.string "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
     t.index ["slug"], name: "index_videos_on_slug", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
