86ab3747e88abce2dffccf24877c9d75d00f3b2b
Adrien POLY <adrienpoly@gmail.com>
2024-04-03 23:42:11 +0200

    add favicon

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,8 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link favicon/site.webmanifest
+//= link favicon/browserconfig.xml
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
