11125d7aa7d91496f52d017d5ae932fe1d4460d6
Adrien Poly <adrienpoly@gmail.com>
2024-04-05 09:02:13 +0200

    add js and css bundling with esbuild (#220)

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,9 +14,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
