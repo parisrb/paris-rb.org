0c7773f0b9deef0464b78a29078c27b67ab8a350
Adrien Poly <adrienpoly@gmail.com>
2024-04-05 09:02:13 +0200

    add js and css bundling with esbuild (#220)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,4 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
 //= link favicon/site.webmanifest
 //= link favicon/browserconfig.xml
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
