cc83b010c6219848702a43f428999f9883caf968
Adrien POLY <adrienpoly@gmail.com>
2024-04-07 23:05:06 +0200

    add tests

--- test/system/admin_test.rb
+++ test/system/admin_test.rb
@@ -1,36 +1,33 @@
-# require "application_system_test_case"
+require "application_system_test_case"
 
-# class AdminTest < ApplicationSystemTestCase
-#   setup do
-#     @admin = users(:admin)
-#   end
+class AdminTest < ApplicationSystemTestCase
+  setup do
+    @admin = users(:admin)
+  end
 
-#   test "sign in admin and visit the admin" do
-#     visit rails_admin.dashboard_path
-#     fill_in "user[email]", with: @admin.email
-#     fill_in "user[password]", with: "password"
-#     click_on "Me connecter"
-#     assert_selector "h1", text: "Administration"
-#   end
+  test "only authenticated admin can access" do
+    visit avo.resources_users_path
+    assert_current_path new_user_session_path
+  end
 
-#   class AuthenticatedAdminTest < ApplicationSystemTestCase
-#     setup do
-#       login_as users(:admin), scope: :user
-#     end
+  class AuthenticatedAdminTest < ApplicationSystemTestCase
+    setup do
+      login_as users(:admin), scope: :user
+    end
 
-#     test "visit the sponsors" do
-#       visit rails_admin.index_path("sponsor")
-#       assert_selector "h1", text: "Liste des Sponsor"
-#     end
+    test "visit the sponsors" do
+      visit avo.resources_sponsors_path
+      assert_selector "[data-target='title'] span", text: "Sponsors"
+    end
 
-#     test "visit the videos" do
-#       visit rails_admin.index_path("video")
-#       assert_selector "h1", text: "Liste des Video"
-#     end
+    test "visit the videos" do
+      visit avo.resources_videos_path
+      assert_selector "[data-target='title'] span", text: "Videos"
+    end
 
-#     test "visit the talks" do
-#       visit rails_admin.index_path("talk")
-#       assert_selector "h1", text: "Liste des Talk"
-#     end
-#   end
-# end
+    test "visit the talks" do
+      visit avo.resources_talks_path
+      assert_selector "[data-target='title'] span", text: "Talks"
+    end
+  end
+end
