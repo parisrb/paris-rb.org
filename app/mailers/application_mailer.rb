762917a5cb9a8ed7ecddedfdae9db9e15d708055
Adrien Poly <adrienpoly@gmail.com>
2024-04-08 05:59:12 +0200

    Replace rubyparis.org with paris-rb.org (#224)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 # app/mailers/application_mailer.rb
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@rubyparis.org"
+  default from: "no-reply@paris-rb.org"
   layout "mailer"
 end
