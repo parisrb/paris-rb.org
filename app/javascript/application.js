68c3ebf12f3da6785593e70b3a77a4c44c774d55
Adrien Poly <adrienpoly@gmail.com>
2024-04-09 23:11:39 +0200

    Videos index, search and pagination (#226)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,18 @@
 import "@hotwired/turbo-rails";
+import "./controllers";
+
+// add the ability to debounce events in the html attributes debounced:keyup->form#submit
+import debounced from "debounced";
+debounced.initialize(debounced.defaultEventNames, { wait: 100 });
+
+// apply view transition on turbo frame
+addEventListener("turbo:before-frame-render", (event) => {
+  if (document.startViewTransition) {
+    const originalRender = event.detail.render;
+    event.detail.render = (currentElement, newElement) => {
+      document.startViewTransition(() =>
+        originalRender(currentElement, newElement)
+      );
+    };
+  }
+});
