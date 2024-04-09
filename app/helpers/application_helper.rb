f62c490557469ba11ff5431d4ef6ddcadf45f520
Adrien Poly <adrienpoly@gmail.com>
2024-04-10 01:48:08 +0200

    Homepage hero (#228)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,16 @@ module ApplicationHelper
       lucide_icon("move-left").concat(content_tag(:span, text, class: "link-text"))
     end
   end
+
+  def external_link_to(text, url = nil, **attributes, &block)
+    if block_given?
+      text = capture(&block)
+      url = text
+    end
+
+    classes = [ class_names("flex items-center gap-2 flex-nowrap"), attributes.delete(:class) ].compact.uniq.join(" ")
+    link_to url, class: classes, target: "_blank", rel: "noopener", **attributes do
+      concat(content_tag(:span, text)).concat(lucide_icon("external-link", class: "w-[1em] h-[1em]"))
+    end
+  end
 end
