d44694cb29e1f9f284410445fc33d1399c2664ae
Adrien POLY <adrienpoly@gmail.com>
2024-04-10 01:44:07 +0200

    fix brakeman offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,9 @@ module ApplicationHelper
     end
   end
 
-  def back_to(text = t("back"), path = :back, **attributes)
-    path = (path == :back) ? "javascript:history.back()" : (params[:back_to].presence || path)
+  def back_to(text = t("back"), path, **attributes)
     classes = [ class_names("flex items-center gap-2"), attributes.delete(:class) ].compact.uniq.join(" ")
+
     link_to path, class: classes, **attributes do
       lucide_icon("move-left").concat(content_tag(:span, text, class: "link-text"))
     end
