153a3ddb87b040b5e12f168a06da8620507f06a0
Adrien Poly <adrienpoly@gmail.com>
2024-04-12 09:01:16 +0200

    Improve dropdown (#231)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,19 +1,19 @@
-import "@hotwired/turbo-rails";
-import "./controllers";
-import "lite-youtube-embed";
+import '@hotwired/turbo-rails'
+import './controllers'
+import 'lite-youtube-embed'
 
 // add the ability to debounce events in the html attributes debounced:keyup->form#submit
-import debounced from "debounced";
-debounced.initialize(debounced.defaultEventNames, { wait: 100 });
+import debounced from 'debounced'
+debounced.initialize(debounced.defaultEventNames, { wait: 100 })
 
 // apply view transition on turbo frame
-addEventListener("turbo:before-frame-render", (event) => {
+window.addEventListener('turbo:before-frame-render', (event) => {
   if (document.startViewTransition) {
-    const originalRender = event.detail.render;
+    const originalRender = event.detail.render
     event.detail.render = (currentElement, newElement) => {
       document.startViewTransition(() =>
         originalRender(currentElement, newElement)
-      );
-    };
+      )
+    }
   }
-});
+})
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,51 +1,51 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./public/*.html",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,html}",
-    "./config/locales/*.yml",
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,html}',
+    './config/locales/*.yml'
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
       },
       container: {
         center: true,
         padding: {
-          DEFAULT: "1rem",
-          sm: "1rem",
-          md: "2rem",
-          lg: "4rem",
-          xl: "5rem",
-          "2xl": "6rem",
-        },
-      },
-    },
+          DEFAULT: '1rem',
+          sm: '1rem',
+          md: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+          '2xl': '6rem'
+        }
+      }
+    }
   },
   daisyui: {
     themes: [
       {
         parisrb: {
-          primary: "#D62828",
-          "primary-content": "#FFFFFF",
-          secondary: "#efe7E9",
-          accent: "#fcd34d",
-          neutral: "#EFE7E9",
-          "base-100": "#FFFFFF",
-          "base-200": "#EFE7E9",
-          "base-300": "#D1CDD1",
-          info: "#bfdbfe",
-          success: "#86efac",
-          warning: "#fde68a",
-          error: "#f87171",
-          "--animation-btn": "none",
-        },
-      },
-    ],
+          primary: '#D62828',
+          'primary-content': '#FFFFFF',
+          secondary: '#efe7E9',
+          accent: '#fcd34d',
+          neutral: '#EFE7E9',
+          'base-100': '#FFFFFF',
+          'base-200': '#EFE7E9',
+          'base-300': '#D1CDD1',
+          info: '#bfdbfe',
+          success: '#86efac',
+          warning: '#fde68a',
+          error: '#f87171',
+          '--animation-btn': 'none'
+        }
+      }
+    ]
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-};
+  plugins: [require('daisyui'), require('@tailwindcss/typography')]
+}
