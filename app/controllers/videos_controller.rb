a502c1be5150bff623f12a76dcd75cf666d33988
Adrien POLY <adrienpoly@gmail.com>
2024-05-01 16:49:29 +0200

    remove legacy expose

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,4 @@
 class VideosController < ApplicationController
-  expose(:video) { Video.where(slug: params[:id]).first }
-
   def index
     @search = Video.ransack(params[:q])
     @videos = @search.result(distinct: true).order(event_date: :desc).page(params[:page])
