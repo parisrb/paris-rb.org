3f78c634645e04453b1600216b7f7c5aee5f27fe
Adrien POLY <adrienpoly@gmail.com>
2024-05-04 08:50:03 +0200

    add a few data on the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include Analytics
   include Internationalisation
   protect_from_forgery with: :exception
+
+  def memory_cache
+    $memory_cache
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
+  before_action :set_counters, only: :index
+
   def index
     @videos = Video.order("created_at DESC").limit(3)
     @sponsors = Sponsor.with_attached_logo.latest(4)
     @lineup_talks = Talk.lineup
+  end
+
+  private
+
+  def set_counters
+    @videos_count = $memory_cache.fetch("videos_count", expires_in: 1.day) { Video.count }
+    @sponsors_count = $memory_cache.fetch("sponsors_count", expires_in: 1.day) { Sponsor.count }
   end
 end
