3b7413ab232cfb9e5f16a480cf237a74d2e7ff88
Adrien POLY <adrienpoly@gmail.com>
2024-05-06 08:56:19 +0200

    clean up

--- app/mailers/talk_mailer.rb
+++ app/mailers/talk_mailer.rb
@@ -1,6 +1,6 @@
 class TalkMailer < ApplicationMailer
   def new_talk(talk)
-    # this is a legacy mail ,the mails are not sent as the Sendgird account is not active
+    # this is a legacy mail, the mails are not sent as the Sendgird account is not active
     # but let'st keep it in case we revive the account
     @talk = talk
     mail to: ENV["EMAIL_RECIPIENTS"].presence || "meetupparisrb@gmail.com",
