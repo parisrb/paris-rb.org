78651c8da955e0fd7a9d1fba10d60b743651143b
Adrien Poly <adrienpoly@gmail.com>
2024-05-08 22:44:42 +0200

    Update vimeo_thumbnail field to compute dynamically youtube urls (#246)

--- app/avo/resources/video.rb
+++ app/avo/resources/video.rb
@@ -7,7 +7,7 @@ class Avo::Resources::Video < Avo::BaseResource
   def fields
     field :id, as: :id
     field :title, as: :text
-    field :vimeo_thumbnail, as: :text, as_html: true do
+    field :vimeo_thumbnail, as: :text, as_html: true, hide_on: [ :forms ] do
       "<img src='#{record.vimeo_thumbnail}' width='100' />"
     end
     field :vimeo_url, as: :text, hide_on: [ :index ]
