eba9e795f395097167f67060c858f5bc1f1a9ab4
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-11-24 21:50:20 +0100

    🚚 Rename video url

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: talks
+#
+#  id                   :bigint           not null, primary key
+#  duration             :string
+#  happened_at          :date
+#  level                :string
+#  preferred_month_talk :string
+#  priority             :string
+#  slides               :string
+#  speaker_email        :string
+#  speaker_name         :string
+#  speaker_twitter      :string
+#  time_position        :datetime
+#  title                :string
+#  video_url            :string
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+# Indexes
+#
+#  index_talks_on_created_at   (created_at)
+#  index_talks_on_happened_at  (happened_at)
+#
 # this sub model is used to display a custom view in the Admin
 class Lineup < Talk
   default_scope { lineup }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_173919) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_24_204014) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -135,8 +135,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_173919) do
   end
 
   create_table "videos", force: :cascade do |t|
-    t.string "vimeo_url"
-    t.string "vimeo_thumbnail"
+    t.string "url"
+    t.string "thumbnail_url"
     t.string "title"
     t.text "description"
     t.date "event_date"
